7171b5d28eb692e7b5415294dceb4dd703394de0
Jimmy Whitaker <jimmymwhitaker@gmail.com>
2020-09-02 18:15:56 -0400

    Changed docker "tag" to "name"

--- pachyderm-github-action/entrypoint.sh
+++ pachyderm-github-action/entrypoint.sh
@@ -5,6 +5,6 @@ echo ${PACHYDERM_TOKEN} | pachctl auth use-auth-token
 for pipeline in  ${PACHYDERM_PIPELINE_FILES}
 do
     # even if the pipeline doesn't exist, this will create it.
-    jq --arg tag ${DOCKER_IMAGE_TAG}  --arg version ${GITHUB_SHA} '.transform.image |= $tag+":"+$version' ${pipeline} | pachctl update pipeline
+    jq --arg tag ${DOCKER_IMAGE_NAME}  --arg version ${GITHUB_SHA} '.transform.image |= $tag+":"+$version' ${pipeline} | pachctl update pipeline
 done
 
