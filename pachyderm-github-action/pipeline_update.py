26e91904ece7959cfda04f2014ebcce0277de33f
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2022-01-21 15:01:44 -0500

    fixed files

--- pachyderm-github-action/pipeline_update.py
+++ pachyderm-github-action/pipeline_update.py
@@ -19,17 +19,16 @@ def setup_client():
         tls = True
     else:
         tls = False
-    return python_pachyderm.Client(host=host, port=port, tls=tls)
+    return python_pachyderm.Client(host=host, port=port, tls=tls, auth_token=pach_token)
 
 
 def create_pipeline_dict(pipeline_files):
     pipelines = {}
     for pipes in pipeline_files:
-        print(pipes)
         if os.path.isfile(pipes):
             f = open(pipes)
             pipe = json.load(f)
-            pipelines.pop({pipe.pipeline.name: pipe})
+            pipelines[pipe["pipeline"]["name"]] = pipe
         elif os.path.isdir(pipes):
             for dirpath, dirs, files in os.walk(pipes):
                 for file in files:
@@ -43,7 +42,6 @@ def get_multi_inputs(pipelinename, input_dict):
     input_list = []
     k = list(input_dict.keys())[0]
     for repo in input_dict[k]:
-        print(repo)
         input_list.append((repo["pfs"]["repo"], pipelinename))
     return input_list
 
@@ -52,7 +50,6 @@ def create_connections(pipelines):
     connections = []
     for k, v in pipelines.items():
         output = k
-        print(v)
         if "pfs" in v["input"]:
             input = v["input"]["pfs"]["repo"]
             connections.append((input, output))
@@ -78,10 +75,12 @@ def sort_pipelines(pipeline_connections):
 
 
 def update_pipeline(pipeline_order, pipelines):
+    pipelines_to_update = [i for i in pipeline_order if i in pipelines.keys()]
     client = setup_client()
-    for pipe in pipeline_order:
-        req = python_pachyderm.parse_dict_pipeline_spec(pipelines[pipe])
-        req["update"] = True
+    for pipe in pipelines_to_update:
+        updated_pipe = pipelines[pipe]
+        updated_pipe["update"] = True
+        req = python_pachyderm.parse_dict_pipeline_spec(updated_pipe)
         client.create_pipeline_from_request(req)
 
 
@@ -90,7 +89,8 @@ def main():
     updated_pipeline = update_image(pipeline_dict, docker_image_name, git_sha)
     conns = create_connections(updated_pipeline)
     pipeline_order = sort_pipelines(conns)
-    update_pipeline(pipeline_order,updated_pipeline)
+    print(pipeline_order)
+    update_pipeline(pipeline_order, updated_pipeline)
 
 
 if __name__ == "__main__":
